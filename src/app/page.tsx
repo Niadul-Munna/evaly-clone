8e02128d50b5e16b1219c552d0b5261c378accc5
niadulMunna <niadulislammunna@gmail.com>
2025-01-30 21:09:39 +0600

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 // import HappyHour from "@/components/happy-hour";
+import FlashSell from "@/components/flash-sell";
+import { FlashSellCarousel } from "@/components/flash-sell-carousel";
 import { Posts } from "@/components/Posts";
 
 export default function Home() {
   return (
     <div>
       {/* <HappyHour /> */}
-      <Posts />
+      {/* <Posts /> */}
+      <FlashSell />
+      <FlashSellCarousel />
     </div>
   );
 }
